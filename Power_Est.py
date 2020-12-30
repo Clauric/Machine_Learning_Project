c623748064558bfe51c26dd1e61868e0806261b8
Clauric <47009424+Clauric@users.noreply.github.com>
2020-12-30 21:50:22 +0000

    Changes to website, flask files

--- Power_Est.py
+++ Power_Est.py
@@ -6,27 +6,27 @@ app = fl.Flask(__name__)                                        # Create a new w
 
 @app.route("/")                                                 # Add root route.
 def home():                                                     # Home page
-    return app.send_static_file("Static/Front_Page.html")            # Return the index.html file
+    return app.send_static_file("Front_Page.html")              # Return the index.html file
 
-@app.route("/api/Lin")                                          # If the Linear Regression button is chosen
+@app.route("/api/Lin/<float:speed>", methods = ["GET"])         # If the Linear Regression button is chosen
 def Lin_Reg(speed):                                             # Call the linear regression function
     lin_model_load = joblib.load("Models/lin_reg.pkl")          # Reimport the linear regression model
-    power_est = np.round(lin_model_load.predict(speed)[0], 3)   # Use the linear regression model to estimate the power for the user inputted speed
-    return {power_est}                                          # Return the power estimate
+    power_est = np.round(lin_model_load.predict([speed])[0], 3) # Use the linear regression model to estimate the power for the user inputted speed
+    return power_est                                            # Return the power estimate
 
-@app.route("/api/RFR")                                          # If the Random Forest Regression button is chosen
+@app.route("/api/RFR/<float:speed>", methods = ["GET"])         # If the Random Forest Regression button is chosen
 def RFR_Reg(speed):                                             # Call the random forest regression function
     RFR_model_load = joblib.load("Models/RFR.pkl")              # Reimport the random forest regression model
     power_est = np.round(RFR_model_load.predict(speed)[0], 3)   # Use the random regression model to estimate the power for the user inputted speed
-    return {power_est}                                          # Return the power estimate
+    return power_est                                            # Return the power estimate
 
-@app.route("/api/DTR")                                          # If the Decision Tree regression button is chosen
+@app.route("/api/DTR/<float:speed>", methods = ["GET"])         # If the Decision Tree regression button is chosen
 def DTR_Reg(speed):                                             # Call the Decision Tree regression function
     DTR_model_load = joblib.load("Models/DTR.pkl")              # Reimport the Decision Tree regression model
     power_est = np.round(DTR_model_load.predict(speed)[0], 3)   # Use the Decision Tree regression model to estimate the power for the user inputted speed
-    return {power_est}                                          # Return the power estimate
+    return power_est                                            # Return the power estimate
 
-@app.route("/api/Lin")                                          # If the Sigmoid Analysis button is chosen
+@app.route("/api/Sig/<float:speed>", methods = ["GET"])         # If the Sigmoid Analysis button is chosen
 def SIG_Reg(speed):                                             # Call the Sigmoid Analysis function
     def sigmoid(x, L ,x0, k, b):                                # Function to define the sigmoid
         y = L / (1 + np.exp(-k*(x-x0)))+b                       # Sigmoid formula
@@ -34,4 +34,4 @@ def SIG_Reg(speed):                                             # Call the Sigmo
 
     popt = np.genfromtxt("Models/popt.csv", delimiter = ",")    # Reimport the optimised values for the sigmoid curve    
     power_est = np.round(sigmoid(speed, *popt)[0], 3)           # Use the Sigmoid Analysis model to estimate the power for the user inputted speed
-    return {power_est}                                          # Return the power estimate
+    return power_est                                            # Return the power estimate
